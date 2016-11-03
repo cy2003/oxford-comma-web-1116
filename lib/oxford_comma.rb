b9cc68822ba5590b65afe6bc2971435425e22cde
Cathy Yee <catyyee@hotmail.com>
2016-11-03 18:58:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+  string = ""
+  if array.length <= 1
+    string = array.join
+  elsif array.length == 2
+    string = array.join(" and ")
+  else array.length > 2
+    string = [array[0...-1].join(", "), array.last].join(", and ")
+  end
+end 
